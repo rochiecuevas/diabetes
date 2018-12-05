5acf534cda4f026ecebd19901b15643ef066b61b
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-04 18:53:40 -0800

    mySQL code

---
+++ Diabetes.sql
@@ -0,0 +1,26 @@
+-- Create a database called diabetes_db --
+DROP DATABASE IF EXISTS diabetes_db;
+CREATE DATABASE diabetes_db;
+
+-- Use diabetes_db to create tables --
+USE diabetes_db;
+
+-- Create tables within the database --
+CREATE TABLE location(
+	ID int primary key not null auto_increment,
+    US_State text
+);
+
+CREATE TABLE obesity(
+	ID int primary key not null auto_increment,
+    `Year`text,
+	US_State text,
+    Obese_Children_Percent float
+);
+
+CREATE TABLE diabetes(
+	ID int primary key not null auto_increment,
+    `Year` text,
+    US_State text,
+    Adult_Diabetics_Percent float
+);
