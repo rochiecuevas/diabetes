c0e58e753b8fd0886197a05542ad95da71ddfcf9
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-05 16:55:41 -0800

    changed route detail

--- diabetes_app.py
+++ diabetes_app.py
@@ -6,14 +6,14 @@ db = pymysql.connect("localhost", "root", f"{password}", "diabetes_db")
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/data')
 def index():
     cursor = db.cursor()
     sql = "SELECT * FROM merged"
     cursor.execute(sql)
     columns = [col[0] for col in cursor.description] # gets the column headers in the merged table
     results = [dict(zip(columns, row)) for row in cursor.fetchall()] # output is a list of dictionaries
-    return render_template("index.html", results = results)
+    return render_template("data.html", results = results)
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
